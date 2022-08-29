c068ce6dff7a12382cada90b5f0dcc096fc1000a
ana sofía <112406721+anasofiamenendez@users.noreply.github.com>
2022-08-29 07:47:12 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+basic.showIcon(IconNames.Happy)
+basic.showIcon(IconNames.Heart)
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.Skull)
+    music.playMelody("G E D C5 A B F C ", 120)
 })
